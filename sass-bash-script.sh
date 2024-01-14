8e8a4f13979550c7e33862e48438338072c50675
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-14 13:28:57 +0330

    sass folder to proejct for styling

--- sass-bash-script.sh
+++ sass-bash-script.sh
@@ -1,6 +1,8 @@
-mkdir base components layout pages themes utils vendors
-cd base
-touch _animations.scss _typography.scss _utilities.scss _base.scss _index.scss
+mkdir sass
+cd sass
+mkdir globals components layout pages themes util vendors
+cd globals
+touch _animations.scss _typography.scss _base.scss _index.scss _colors.scss _boilerplate.scss
 cd .. 
 cd components 
 touch _index.scss
@@ -9,14 +11,14 @@ cd layout
 touch _index.scss _flexbox.scss _grid.scss
 cd .. 
 cd pages
-touch _index.scss _home.scss _contact.scss # Any type of page you want can make it
+touch _index.scss _home.scss # Any type of page you want can make it
 cd ..
 cd themes
 touch _index.scss _dark.scss _default.scss
 cd ..
-cd utils
-touch _index.scss _functions.scss _mixins.scss _variables.scss _breakpoints.scss _gaps.scss
+cd util
+touch _index.scss _functions.scss _mixins.scss _variables.scss _breakpoints.scss _gaps.scss _fonts.scss
 cd .. 
 cd vendors
-touch _bootstrap.scss _jquery-ui.scss
-cd ..
+touch _bootstrap.scss _jquery-ui.scss _index.scss
+cd ../../
