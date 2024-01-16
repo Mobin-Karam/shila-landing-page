413f669beda6d4ceb73a773a8873ef42a3ecae08
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-16 18:18:34 +0330

    fix some bug in login form

--- js/script.js
+++ js/script.js
@@ -2,20 +2,25 @@ const loginButton = document.getElementById("login-button");
 const modalBox = document.getElementById("modal");
 const modalBoxSmall = document.getElementById("modal-small");
 const exitModal = document.getElementById("exit-modal");
+const mobileInput = document.getElementById("mobile");
 
+// Modal Login Form
+// If window Width < 900 Show this
 if (window.innerWidth < 900) {
   // Login Button
   loginButton.addEventListener("click", () => {
-    modalBox.style.animation = "popup 0.6s ease-in-out 0s 1 alternate";
-    modalBox.style.display = "block";
+    modalBoxSmall.style.animation = "popup 0.6s ease-in-out 0s 1 alternate";
+    modalBox.style.display = "flex";
   });
   // Exit Button From Login Form
   exitModal.addEventListener("click", () => {
-    modalBox.style.animation = "popdown 0.6s ease-in-out 0s 1 alternate";
+    modalBoxSmall.style.animation = "popdown 0.6s ease-in-out 0s 1 alternate";
     setTimeout(() => {
       modalBox.style.display = "none";
     }, 600);
   });
+
+  // If window Width > 900 Show this
 } else if (window.innerWidth >= 900) {
   // Login Button
   loginButton.addEventListener("click", () => {
@@ -31,7 +36,8 @@ if (window.innerWidth < 900) {
   });
 }
 
-console.log(window.innerWidth < 900);
+
+// Mobile Input
 
 
-// api 
+
