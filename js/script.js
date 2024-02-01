7ba7f8dea4244fb59b0859d1b5b950adce59ef2b
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-01 09:20:50 +0330

    complete items box components

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-import api from "./api.js";
+// import api from "./api.js";
 
-// API call
-console.log(api());;
+let response = await fetch(
+  "https://restaurant.delino.com/restaurant/menu/00b99096-3dca-41dd-ae68-61265cafd5ca"
+);
 
-// Main Content Change wiht Nav
+let json;
+
+if (response.ok) {
+  // if HTTP-status is 200-299
+  // get the response body (the method explained below)
+  json = await response.json();
+} else {
+  console.log("HTTP-Error: " + response.status);
+}
+
+// const foodTypes = json.categories.slice().map((obj) => {
+//   let title = obj.title;
+//   let foods = obj.sub[0].food;
+//   let foodImg = obj.sub[0].food[0].img.replace("#SIZEOFIMAGE#", "560x350");
+
+//   const boxType = (document.getElementById(
+//     "box1"
+//   ).style.backgroundImage = `url(${obj.sub[0].food[1].img.replace(
+//     "#SIZEOFIMAGE#",
+//     "560x350"
+//   )})`);
+
+//   return {
+//     title: title,
+//     foods: foods,
+//     foodImg: foodImg,
+//   };
+// });
+
+// console.log(foodTypes);
+console.log(json.categories);
+
+// Main Content Change with Nav
 const menus = document.querySelectorAll(".menu");
 menus[1].classList.add("active");
 
