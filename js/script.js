ae6c0575e83230d3e4ae9a7af0f667c896322eef
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-02-03 16:02:52 +0330

    add interactive to content items

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 // import api from "./api.js";
 
-let response = await fetch(
-  "https://restaurant.delino.com/restaurant/menu/00b99096-3dca-41dd-ae68-61265cafd5ca"
-);
+// let response = await fetch(
+//   "https://restaurant.delino.com/restaurant/menu/00b99096-3dca-41dd-ae68-61265cafd5ca"
+// );
 
-let json;
+// let json;
 
-if (response.ok) {
-  // if HTTP-status is 200-299
-  // get the response body (the method explained below)
-  json = await response.json();
-} else {
-  console.log("HTTP-Error: " + response.status);
-}
+// if (response.ok) {
+//   // if HTTP-status is 200-299
+//   // get the response body (the method explained below)
+//   json = await response.json();
+// } else {
+//   console.log("HTTP-Error: " + response.status);
+// }
 
 // const foodTypes = json.categories.slice().map((obj) => {
 //   let title = obj.title;
@@ -34,7 +34,7 @@ if (response.ok) {
 // });
 
 // console.log(foodTypes);
-console.log(json.categories);
+// console.log(json.categories);
 
 // Main Content Change with Nav
 const menus = document.querySelectorAll(".menu");
@@ -49,3 +49,10 @@ menus[1].addEventListener("click", () => {
   menus[0].classList.remove("active");
   menus[1].classList.add("active");
 });
+
+const box = document.querySelectorAll(".box");
+let element = [];
+for (let i = 0; i < box.length; i++) {
+  element.push(box[i]);
+  box[i].addEventListener("click", () => {});
+}
